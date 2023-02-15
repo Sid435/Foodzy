3ad8d3027bca6507b291ede2e6a42a76e0d930e6
Musketeers <tmusketeers432@gmail.com>
2023-02-15 15:55:56 +0530

    Dragonite mein tumhe chunta hu!!!

--- app/src/main/java/com/example/foodzy/cart_details.java
+++ app/src/main/java/com/example/foodzy/cart_details.java
@@ -92,7 +92,7 @@ public class cart_details extends AppCompatActivity {
                 catch (Exception ignored){}
 
 
-                payusingupi("Name","paytm-27619993@paytm","", final_CARTprice_final.toString());
+                payusingupi("Name","airtel66.payu@icici","", "1");
             }
         });
 
